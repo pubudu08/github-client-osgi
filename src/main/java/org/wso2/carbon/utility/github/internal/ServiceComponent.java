7d5172506297e5448f8710779380c759a4de0990
Pubudu Dissanayake <pubudu08@gmail.com>
2014-09-15 11:11:36 +0530

    Codereview: Refactored Exception Handling

--- src/main/java/org/wso2/carbon/utility/github/internal/ServiceComponent.java
+++ src/main/java/org/wso2/carbon/utility/github/internal/ServiceComponent.java
@@ -5,34 +5,34 @@ import org.apache.commons.logging.LogFactory;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceRegistration;
 import org.osgi.service.component.ComponentContext;
-import org.wso2.carbon.utility.versioncontrol.IRepository;
 import org.wso2.carbon.utility.github.service.RepositoryAdminService;
-
+import org.wso2.carbon.utility.versioncontrol.VersionControlArtifact;
 
 
 /**
-* @scr.component name="org.wso2.carbon.utility.github"
-* immediate="true"
-*/
+ * @scr.component name="org.wso2.carbon.utility.github"
+ * immediate="true"
+ */
 public class ServiceComponent {
 	private ServiceRegistration registration;
-	
+
 	private static RepositoryAdminService adminService;
 	private static BundleContext bundleContext;
-    private static final Log logger = LogFactory.getLog(ServiceComponent.class);
+	private static final Log logger = LogFactory.getLog(ServiceComponent.class);
 
 	protected void activate(ComponentContext context) {
-        logger.info("Version Control Service: GitHub bundle is activated");
-        adminService = new RepositoryAdminService();
-        bundleContext = context.getBundleContext();
-        registration = context.getBundleContext().registerService(IRepository.class.getName(),adminService, null);
-    }
+		logger.info("Version Control Service: GitHub bundle is activated");
+		adminService = new RepositoryAdminService();
+		bundleContext = context.getBundleContext();
+		registration = context.getBundleContext().registerService(
+		  VersionControlArtifact.class.getName(), adminService, null);
+	}
 
-    protected void deactivate(ComponentContext context) {
-        logger.info("Version Control Service: GitHub bundle is deactivated");
-        registration.unregister();
-        adminService = null;
-        bundleContext = null;
-    }
+	protected void deactivate(ComponentContext context) {
+		logger.info("Version Control Service: GitHub bundle is deactivated");
+		registration.unregister();
+		adminService = null;
+		bundleContext = null;
+	}
 
 }
--- src/main/java/org/wso2/carbon/utility/github/service/RepositoryAdminService.java
+++ src/main/java/org/wso2/carbon/utility/github/service/RepositoryAdminService.java
@@ -4,61 +4,70 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.eclipse.egit.github.core.Repository;
 import org.eclipse.egit.github.core.service.RepositoryService;
-import org.wso2.carbon.utility.versioncontrol.IRepository;
+import org.wso2.carbon.utility.versioncontrol.VersionControlArtifact;
+import org.wso2.carbon.utility.versioncontrol.exception.GenericArtifactException;
 
 import java.io.IOException;
 import java.util.List;
 
-public class RepositoryAdminService implements IRepository {
 
-    private static final Log logger = LogFactory.getLog(RepositoryAdminService.class);
-	
-	public boolean createRepository(String username, String password, String repositoryName){
-		try{
+public class RepositoryAdminService implements VersionControlArtifact {
 
-        	RepositoryService service = new RepositoryService();
-    		service.getClient().setCredentials(username, password);
-    		Repository repo = new Repository();
-            repo.setName(repositoryName);
-        	service.createRepository(repo);
-            logger.info(repo.getName()+" repository has been created ! ");
+	private static final Log LOGGER = LogFactory.getLog(RepositoryAdminService.class);
 
-            List<Repository> listOfRepo = null;
-            listOfRepo = service.getRepositories();
-            for (Repository repository : listOfRepo) {
-                System.out.println(repository.getName());
-                if(repository.getName().equals(repositoryName)){
-                    return true;
-                }
-            }
-
-            return true;
-        }catch (IOException e){
-        	return false;
-        }
-	}
-	
-	public boolean isRepositoryExist(String username, String password, String repositoryName){
-		RepositoryService service = new RepositoryService();
-		service.getClient().setCredentials(username, password);
-		
-		// Here need to add naming logic
-		repositoryName  = repositoryName.replaceAll(" ", "-");
-		List<Repository> listOfRepo = null;
+	/**
+	 * To Create new GitHub project.
+	 *
+	 * @param username
+	 * @param password
+	 * @param repositoryName
+	 * @throws IOException
+	 * @throws GenericArtifactException
+	 */
+	public void createRepository(String username, String password, String repositoryName)
+	  throws GenericArtifactException {
 		try {
-			listOfRepo = service.getRepositories();
-		} catch (IOException e) {
-			return false;
-		}
-        for (Repository repository : listOfRepo) {
-        	//System.out.println(repository.getName());
-        	if(repository.getName().equals(repositoryName)){
-        		return true;
+			RepositoryService service = new RepositoryService();
+			service.getClient().setCredentials(username, password);
+			// Here need to add naming logic
+			repositoryName = repositoryName.replaceAll(" ", "-");
+			List<Repository> listOfRepo;
+			try {
+				listOfRepo = service.getRepositories();
+			} catch (IOException exception) {
+				LOGGER.error(" ");
+				throw new GenericArtifactException("Unable to create the repository",
+				  exception, "Repository_Creation_Failed");
 			}
-        }        
-		return false;
+			for (Repository repository : listOfRepo) {
+				if (repository.getName().equals(repositoryName)) {
+					throw new GenericArtifactException(
+					  "Repository is already exists, Please provide" +
+					  "a suitable Repository name. ", "Repository_Already_Exists");
+				}
+			}
+			Repository repo = new Repository();
+			repo.setName(repositoryName);
+			service.createRepository(repo);
+			listOfRepo = service.getRepositories();
+			for (Repository repository : listOfRepo) {
+				if (repository.getName().equals(repositoryName)) {
+					if (LOGGER.isDebugEnabled()) {
+						LOGGER.debug(repo.getName() + " repository has been created");
+					}
+				}
+			}
+		} catch (IOException exception) {
+			throw new GenericArtifactException("Unable to create the repository",
+			  exception, "Repository_Creation_Failed");
+		}
 	}
 
+	/**
+	 * To retrieve repository type.
+	 *
+	 * @return github
+	 */
 	public String getRepositoryType() {
 		return "github";
 	}
